6942b1646b6ff355a7ec5d89ea0de3847abae9f5
Bera <berakoc97@gmail.com>
2020-10-25 21:27:21 +0300

    🌌 MODIFY: Implemented Component.js

--- src/Component.js
+++ src/Component.js
@@ -1,3 +1,33 @@
-export const f = () => {
-    console.log('I am a component')
+import React, {useEffect, useRef} from 'react'
+import PropTypes from 'prop-types'
+
+OnShow.propTypes = {
+    handler: PropTypes.func.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]).isRequired
+}
+
+export class OnShowHandlerIsNotFound extends Error {
+    constructor(message='Handler function for onShow event is not defined.') {
+        super(message)
+        this.name = OnShowHandlerIsNotFound.name
+    }
+}
+
+export function OnShow(props) {
+    const onShowFunc = onShow ? onShow : () => throw new OnShowHandlerIsNotFound()
+    const ref = useRef(null)
+    useEffect(() => {
+        onShowFunc(ref.current, props.handler)
+        return () => {
+            window.removeEventListener('onScroll', props.handler)
+        }
+    }, [])
+    return (
+        <div ref={ref}>
+            {props.children}
+        </div>
+    )
 }
